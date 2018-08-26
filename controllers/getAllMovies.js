53de3e0144530ee4088f154179e1073d0959b5fa
gokulkrishh <krishnangokul9@gmail.com>
2018-08-26 09:59:06 +0530

    Updated api's

--- controllers/getAllMovies.js
+++ controllers/getAllMovies.js
@@ -2,11 +2,10 @@ import allMovies from "../data/movies.json";
 import { generateActorNames, generateNumberArray, generateRandomGenres, generateAllMovieData } from "../utils";
 
 const getAllMovies = (count = 10) => {
-  if (count > 40) count = 40;
+  if (count > 40) count = 41;
   const moviesCount = generateNumberArray(count);
   return moviesCount.map((movieCount, index) => {
-    const movie = allMovies[movieCount];
-    movie.id = movieCount;
+    const movie = allMovies.filter(movie => movie.id === id);
     return generateAllMovieData(movie);
   });
 };
--- controllers/getMovieById.js
+++ controllers/getMovieById.js
@@ -2,10 +2,9 @@ import allMovies from "../data/movies.json";
 import { generateMovieData, generateActorNames } from "../utils";
 
 const getMovieById = (id) => {
-  const movie = allMovies[id];
+  const movie = allMovies.filter(movie => movie.id === id);
   if (!movie) return [];
   else {
-    movie.id = id;
     return generateMovieData(movie);
   }
 };
