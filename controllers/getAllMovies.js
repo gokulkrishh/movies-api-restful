f9e6f0e3732ddea5277193fc26a6722b16856c62
gokulkrishh <krishnangokul9@gmail.com>
2018-08-27 19:03:46 +0530

    Updated readme

--- controllers/getAllMovies.js
+++ controllers/getAllMovies.js
@@ -1,7 +1,7 @@
 import allMovies from "../data/movies.json";
 import { generateNumberArray, generateAllMovieData } from "../utils";
 
-const getAllMovies = (count = 41) => {
+const getAllMovies = (count = allMovies.length) => {
   const moviesCount = generateNumberArray(count);
   return moviesCount.map(movieCount => generateAllMovieData(allMovies[movieCount]));
 };
